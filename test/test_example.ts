983fd59c1be93378130e21c3ee88f77b1043add6
Mike Lueders <mike.lueders@shippo.com>
2024-05-29 12:31:42 -0500

    update spec and test

--- test/test_example.ts
+++ test/test_example.ts
@@ -1,7 +1,8 @@
 import fetchMock from 'fetch-mock';
-import { describe, it } from 'mocha';
-import { Shippo } from '../sdk';
+import {describe, it} from 'mocha';
+import {Shippo} from '../sdk';
 import {GetExampleResponse} from "../models/operations";
+import {expect} from "chai";
 
 // TODO: figure out why this is required
 Object.assign(fetchMock.config, { Headers, Request, Response, fetch })
@@ -10,22 +11,40 @@ globalThis.fetch = fetchMock.sandbox();
 describe('TestExample', function() {
     this.timeout(10000);
 
-    it('test_example', async () => {
+    beforeEach(() => {
+        fetchMock.reset()
+    })
+
+    it('test example simple', async () => {
         fetchMock.get('http://localhost:8080/example', {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
             body: {
-                field: 'Hello, world!',
+                field: 'value',
             }
         });
 
         const sdk = new Shippo({serverURL: "http://localhost:8080"})
 
-        try {
-            const result: GetExampleResponse = await sdk.getExample()
-            console.log(result.exampleBody)
-        } catch (err) {
-            console.log(err)
-        }
+        const result: GetExampleResponse = await sdk.getExample()
+        expect(result.exampleBody.field).to.be.equal('value')
+    })
+
+    it('test example complex', async () => {
+
+        fetchMock.get('http://localhost:8080/example', {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: {
+                field: {
+                    other_field: 'value'
+                },
+            }
+        });
+
+        const sdk = new Shippo({serverURL: "http://localhost:8080"})
+
+        const result: GetExampleResponse = await sdk.getExample()
+        expect(result.exampleBody.field.otherField).to.be.equal('value')
     })
 });
