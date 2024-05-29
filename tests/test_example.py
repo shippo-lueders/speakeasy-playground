3463428d6a15b5f1ed059127f056e1eb5e7a8aae
Mike Lueders <mike.lueders@shippo.com>
2024-05-29 12:12:27 -0500

    update spec and test

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,8 @@ import httpretty
 import pytest
 import shippo
 
+from src.shippo.models.components import ExampleObject
+
 BASE_URL = "http://localhost:9500"
 
 
@@ -12,11 +14,21 @@ class TestExample:
         return shippo.Shippo(server_url=BASE_URL)
 
     @httpretty.activate
-    def test_example(self, api: shippo.Shippo):
+    def test_field_simple(self, api: shippo.Shippo):
         httpretty.register_uri(
             httpretty.GET, f"{BASE_URL}/example", status=200, content_type="application/json",
             body='{"field": "value"}'
         )
 
         response = api.example()
-        print(response)
+        assert isinstance(response.field, str)
+
+    @httpretty.activate
+    def test_field_complex(self, api: shippo.Shippo):
+        httpretty.register_uri(
+            httpretty.GET, f"{BASE_URL}/example", status=200, content_type="application/json",
+            body='{"field": {"other_field": "value"}}'
+        )
+
+        response = api.example()
+        assert type(response.field).__name__ == ExampleObject.__name__
