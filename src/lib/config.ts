cfd1cb7c109f83964084f803e0fc30a257342527
Mike Lueders <mike.lueders@shippo.com>
2024-06-24 15:40:25 -0500

    generate

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -47,7 +47,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
     language: "typescript",
     openapiDocVersion: "1",
-    sdkVersion: "0.4.0",
+    sdkVersion: "0.5.0",
     genVersion: "2.352.0",
-    userAgent: "speakeasy-sdk/typescript 0.4.0 2.352.0 1 shippo",
+    userAgent: "speakeasy-sdk/typescript 0.5.0 2.352.0 1 shippo",
 } as const;
